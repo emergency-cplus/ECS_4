ec5ed684c6dab748d328f029d9df1a819b76429c
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-05-25 02:18:23 +0900

    アイテムビューの調整、mockupの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,26 +5,14 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  theme: {
-    extend: {
-      colors: {
-        green: {
-          500: '#38a169',
-          600: '#2f855a',
-        },
-        blue: {
-          500: '#4299e1',
-          600: '#3182ce',
-        },
-      },
-    },
-  },
+
   plugins: [
-    require('daisyui')
+    require('daisyui'),
+    require('@tailwindcss/typography')
   ],
   daisyui: {
     themes: [
-      "cmyk"
+      "emerald",
     ],
   },
 }
