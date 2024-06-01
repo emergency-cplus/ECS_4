dff8b2dabad6f81e979108fad2fda226c11bc305
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-02 02:21:16 +0900

    ヘッダーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :require_login
     add_flash_types :success, :danger
+    before_action :set_locale
+
+    def set_locale
+        I18n.locale = params[:locale] || I18n.default_locale
+    end
     
     private
 
