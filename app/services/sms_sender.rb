2010512cfc7a7513fc84ca7b79afea9490939209
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-17 20:39:07 +0900

    サービスクラスの叩き台を作成しました

--- app/services/sms_sender.rb
+++ app/services/sms_sender.rb
@@ -3,11 +3,12 @@ class SmsSender
     @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
   end
 
+  # SMSを送信するメソッド
   def send_sms(to:, body:)
     @client.messages.create(
-      from: ENV['TWILIO_PHONE_NUMBER'],
-      to: to,
-      body: body
+      from: ENV['TWILIO_PHONE_NUMBER'], # 送信元の電話番号
+      to: to,                           # 送信先の電話番号
+      body: body                        # 送信するメッセージ本文
     )
   end
 end
