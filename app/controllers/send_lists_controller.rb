f09cf7d312850b390a88bcfdc40e33f01055257f
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-21 06:00:06 +0900

    送信エラー時のメッセージを修正

--- app/controllers/send_lists_controller.rb
+++ app/controllers/send_lists_controller.rb
@@ -15,35 +15,38 @@ class SendListsController < ApplicationController
 
   def create
     if params[:phone_number].length != 11 || params[:phone_number].scan(/\D/).any?
-      # 電話番号が11桁でない、または数字以外の文字が含まれている場合
       flash[:danger] = '入力に誤りがあります。電話番号はハイフンなしの11桁の数字を入力してください。'
       redirect_to items_path and return
     end
-
+  
     sms_sender = SmsSender.new
     item = Item.find(params[:item_id])
     user_message = User.find(item.user_id).message_template
     full_body = "#{user_message} Check this out: #{item.item_url}"
-
-    response = sms_sender.send_sms(
-      to: params[:phone_number],
-      body: full_body
-    )
-
-    if response.status == 'queued'
-      SendList.create(
-        phone_number: params[:phone_number],
-        send_at: Time.zone.now,
-        sender: params[:sender_name],
-        item_id: item.id,
-        user_id: current_user.id
+  
+    begin
+      response = sms_sender.send_sms(
+        to: params[:phone_number],
+        body: full_body,
+        item: item  # ここで item 引数を渡します
       )
-      redirect_to send_lists_path, success: 'SMSを送信しました。'
-    else
-      redirect_to send_lists_path, danger: 'SMSの送信に失敗しました。'
+      if response.status == 'queued'
+        SendList.create(
+          phone_number: params[:phone_number],
+          send_at: Time.zone.now,
+          sender: params[:sender_name],
+          item_id: item.id,
+          user_id: current_user.id
+        )
+        redirect_to send_lists_path, success: 'SMSを送信しました。'
+      else
+        redirect_to send_lists_path, danger: 'SMSの送信に失敗しました。'
+      end
+    rescue StandardError => e
+      logger.error "SMS sending failed: #{e.message}"
+      redirect_to send_lists_path, danger: 'SMS送信中にエラーが発生しました。'
     end
   end
-
   def update; end
 
   private
