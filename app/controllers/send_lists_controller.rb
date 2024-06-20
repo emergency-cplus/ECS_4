3e04db00b2feb9d5e8bec5ee80f107081038923f
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-20 13:50:25 +0900

    ハイフンなしの11桁の電話番号を保証するためのバリデーションを追加

--- app/controllers/send_lists_controller.rb
+++ app/controllers/send_lists_controller.rb
@@ -14,37 +14,45 @@ class SendListsController < ApplicationController
   def edit; end
 
   def create
-  sms_sender = SmsSender.new
-  item = Item.find(params[:item_id])
+    if params[:phone_number].length != 11 || params[:phone_number].scan(/\D/).any?
+      # 電話番号が11桁でない、または数字以外の文字が含まれている場合
+      flash[:danger] = '入力に誤りがあります。電話番号はハイフンなしの11桁の数字を入力してください。'
+      redirect_to items_path and return
+    end
 
-  response = sms_sender.send_sms(
-    to: params[:phone_number],
-    body: "#{User.find(item.user_id).message_template} Check this out: #{item.item_url}",
-    item: item
-  )
+    sms_sender = SmsSender.new
+    item = Item.find(params[:item_id])
+    user_message = User.find(item.user_id).message_template
+    full_body = "#{user_message} Check this out: #{item.item_url}"
 
-  if response.status == 'queued'
-    SendList.create(
-      phone_number: params[:phone_number],
-      send_at: Time.zone.now,
-      sender: params[:sender_name],
-      item_id: item.id,
-      user_id: current_user.id
+    response = sms_sender.send_sms(
+      to: params[:phone_number],
+      body: full_body
     )
-    redirect_to send_lists_path, success: 'SMSを送信しました。'
-  else
-    redirect_to send_lists_path, alert: 'SMSの送信に失敗しました。'
+
+    if response.status == 'queued'
+      SendList.create(
+        phone_number: params[:phone_number],
+        send_at: Time.zone.now,
+        sender: params[:sender_name],
+        item_id: item.id,
+        user_id: current_user.id
+      )
+      redirect_to send_lists_path, notice: 'SMSを送信しました。'
+    else
+      redirect_to send_lists_path, alert: 'SMSの送信に失敗しました。'
+    end
   end
-end
 
   def update; end
 
   private
-    def set_send_list
-      @send_list = SendList.find(params[:id])
-    end
 
-    def send_list_params
-      params.require(:send_list).permit(:phone_number, :sender_name, :item_id, :user_id, :send_at)
-    end
+  def set_send_list
+    @send_list = SendList.find(params[:id])
+  end
+
+  def send_list_params
+    params.require(:send_list).permit(:phone_number, :sender_name, :item_id, :user_id, :send_at)
+  end
 end
