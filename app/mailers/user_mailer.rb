e3e5a3beab1564bafd5570be71219d4004c627fd
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-24 05:13:25 +0900

    メーラーの作成をしました

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,11 @@
 class UserMailer < ApplicationMailer
-  default from: 'your-email@example.com'
+  # メールアドレスは環境変数から取得する
+  default from: ENV['RESET_PASSWORD_EMAIL'] || 'default-email@example.com'
 
   # パスワードリセットのメールを送信するメソッド
   def password_reset_email(user)
     @user = user
     @token = user.reset_password_token # Sorceryで生成されるトークン
-    mail(to: @user.email, subject: 'パスワードリセットの指示')
+    mail(to: @user.email, subject: 'パスワードリセット')
   end
 end
