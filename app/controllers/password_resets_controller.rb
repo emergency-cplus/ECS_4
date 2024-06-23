e35c3a6173194233c6df290e13c740baf7d476c8
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-24 05:50:47 +0900

    PasswordResets コントローラにパスワードリセットのロジックを実装

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,13 +1,48 @@
 class PasswordResetsController < ApplicationController
-  def new
+  skip_before_action :require_login
+
+  def new; end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+    unless @user
+      redirect_to new_password_reset_path, alert: '無効または期限切れのトークンです。もう一度試してください。'
+    end
   end
 
   def create
-  end
-
-  def edit
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path, notice: 'パスワードリセットのメールを送信しました。メールをご確認ください。'
   end
 
   def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+
+    if @user.blank?
+      redirect_to new_password_reset_path, alert: '無効または期限切れのトークンです。もう一度試してください。'
+      return
+    end
+
+    if valid_password_params?(params[:user][:password], params[:user][:password_confirmation])
+      if @user.change_password(params[:user][:password])
+        redirect_to login_path, notice: 'パスワードがリセットされました。新しいパスワードでログインしてください。'
+      else
+        flash.now[:error] = 'パスワードリセットに失敗しました。もう一度試してください。'
+        render :edit
+      end
+    else
+      flash.now[:error] = 'パスワードは最低6文字で、数字と大文字を含む必要があります。'
+      render :edit
+    end
+  end
+
+  private
+
+  # パスワードバリデーションをチェックするメソッド
+  def valid_password_params?(password, confirmation)
+    password == confirmation && password.length >= 6 && password.match?(/\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+\z/)
   end
 end
