c6ae3a345dd66bd08bdcea30fcb59670d9e5299c
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-06-28 18:46:52 +0900

    マイグレーションファイルのロールバック

--- db/migrate/20240627121316_enable_uuid_extension.rb
+++
@@ -1,5 +0,0 @@
-class EnableUuidExtension < ActiveRecord::Migration[7.0]
-  def change
-    enable_extension 'pgcrypto' # PostgreSQLのpgcrypto拡張を有効に
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_121316) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_213944) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
