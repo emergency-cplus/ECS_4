cd2852ce3795de873d6721a2c27b5f873195eb65
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-07-14 06:31:24 +0900

    itemモデルにnullifyオプションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :send_lists
+  has_many :send_lists, dependent: :nullify
 
   validates :title, presence: true, length: { maximum: 255 }
-  validates :item_url, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :item_url, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: true }
   # 説明欄は必須ではない
   validates :description, length: { maximum: 255 }, allow_blank: true
   
