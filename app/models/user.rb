9def4b7906e232b5eda185b20d092e0e76c4cc2a
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-07-17 21:50:54 +0900

    パスワードのバリデーションを強化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,18 @@ class User < ApplicationRecord
 
   has_many :items
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  # 強化されたパスワードバリデーション
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)(?=.*?[!@#$%^&*\(\)\-_\+=\{\}\[\]|:;'",<>\.?\/~]).{8,}\z/, message: "は半角の英数字および記号をそれぞれ1つ以上含む必要があります（大文字小文字は区別)
+  " }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :reset_password_token, uniqueness: true, allow_nil: true
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 255 }
 
-  # UUIDを生成して確保する
   before_create :ensure_uuid
 
-  # パスワードリセットトークンを生成し、メールを送信するメソッド
-  # ※ このメソッドは、Sorceryのデフォルトのメール送信機能をオーバーライドしています
   def send_password_reset_email
     self.reset_password_token = SecureRandom.urlsafe_base64
     self.reset_password_sent_at = Time.zone.now
