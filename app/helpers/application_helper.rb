acb47b0ef9f1fd10624fb27cc49ffb1526aa4909
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-10-01 06:24:56 +0900

    重複したブランチの本体の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,20 @@ module ApplicationHelper
   def flash_background_color(type)
     case type.to_sym
     when :success then "bg-info"   # 成功メッセージ用
-    when :danger then "bg-accent"      # 警告メッセージ用
-    when :error then "bg-gray-500"      # エラーメッセージ用
-    else "bg-gray-500"                 # その他のメッセージ用
+    when :danger then "bg-accent"  # 警告メッセージ用
+    else "bg-gray-500"             # エラーメッセージ用とその他のメッセージ用
     end
   end
 
   def mask_phone_number(phone_number)
     phone_number.gsub(/(\d{3})\d*(\d{2})$/, '\1******\2')
+  end
+
+  def message_background_color(message)
+    case message
+    when 'notice' then 'bg-blue-500'
+    when 'alert' then 'bg-red-500'
+    else 'bg-gray-500'
+    end
   end
 end
