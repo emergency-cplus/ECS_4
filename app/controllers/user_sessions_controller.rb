ff8dde34b2764efdc4e5802ba67315ee64ec0545
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-11-15 11:02:45 +0900

    管理者認証のコード修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
     if @user
       @user.increment_login_count!
-      redirect_back_or_to root_path, success: "ログインしました"
+      after_login_redirect
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
@@ -16,6 +16,8 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
+    # ログアウト前に現在のユーザーのroleを保存しておく（必要に応じて）
+    was_admin = current_user&.admin?
     logout
     redirect_to root_path, success: "ログアウトしました"
   end
@@ -23,6 +25,17 @@ class UserSessionsController < ApplicationController
   private
 
   def redirect_if_logged_in
-    redirect_to root_path, info: "すでにログインしています" if logged_in?
+    if logged_in?
+      redirect_path = current_user.admin? ? admin_top_path : root_path
+      redirect_to redirect_path, info: "すでにログインしています"
+    end
+  end
+
+  def after_login_redirect
+    if @user.admin?
+      redirect_to admin_top_path, success: "管理者としてログインしました"
+    else
+      redirect_back_or_to root_path, success: "ログインしました"
+    end
   end
 end
