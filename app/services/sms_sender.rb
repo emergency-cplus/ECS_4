9a79184b9dee57eec3339cd31915d069c92c1dbd
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-12-02 13:07:50 +0900

    test送信と本番の分離

--- app/services/sms_sender.rb
+++ app/services/sms_sender.rb
@@ -1,28 +1,55 @@
 class SmsSender
   def initialize
-    @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    @client = Twilio::REST::Client.new(
+      ENV['TWILIO_ACCOUNT_SID'],
+      ENV['TWILIO_AUTH_TOKEN']
+    )
   end
 
-  def send_sms(to:, body:, item:)
+  def send_sms(to:, body:, item:, is_test: false)
     formatted_phone_number = format_phone_number(to)
     return unless formatted_phone_number
 
-    user_message = User.find(item.user_id).message_template
-    full_body = "#{user_message} #{item.item_url} 身に覚えのない場合は無視してください。"
-
-    @client.messages.create(
-      from: ENV['TWILIO_PHONE_NUMBER'],
-      to: formatted_phone_number,
-      body: full_body
-    )
+    begin
+      Rails.logger.info "Twilioクレデンシャル確認: SID=#{ENV['TWILIO_ACCOUNT_SID']&.first(6)}..."
+      Rails.logger.info "送信元番号: #{ENV['TWILIO_PHONE_NUMBER']}"
+      
+      # メッセージ本文を構築
+      message_body = build_message(item, is_test)
+      
+      response = @client.messages.create(
+        from: ENV['TWILIO_PHONE_NUMBER'],
+        to: formatted_phone_number,
+        body: message_body
+      )
+      
+      Rails.logger.info "Twilioレスポンス: #{response.status}"
+      response
+      
+    rescue => e
+      Rails.logger.error "Twilioエラー詳細: #{e.class}: #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      raise
+    end
   end
 
   private
 
-  # 電話番号を国際形式にフォーマット
-  def format_phone_number(phone_number)
-    phone_number = phone_number.gsub(/\D/, '') # 数字のみ取り出す
-    return nil unless phone_number.length == 10 || phone_number.length == 11
-    "+81#{phone_number[1..]}"
+  def format_phone_number(number)
+    return unless number.present?
+    number = number.gsub(/[-\s]/, '')
+    number = "+81#{number[1..-1]}" if number.start_with?('0')
+    number
+  end
+
+  def build_message(item, is_test)
+    # テストメッセージの場合は先頭に[テスト]を付ける
+    prefix = is_test ? "[テスト] " : ""
+    
+    # メッセージ本文を構築
+    message = "#{prefix}#{item.user.name}から送信されました。\n"
+    message += "#{item.item_url}"
+
+    message
   end
 end
